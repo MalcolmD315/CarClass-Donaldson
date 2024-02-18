5ee2b46741fec4611778a28ed8c1cefa674797b6
MalcolmD315 <159967905+MalcolmD315@users.noreply.github.com>
2024-02-18 14:02:54 -0500

    Created UI Interface
    Added UI elements for player input.
    Created script for gameplay

---
+++ Assets/Gameplay.cs
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Car Class
+//Name: Malcolm Donaldson
+//Section: SGD.213.2172
+//Instructor: Brian Sowers
+//Date: 2/13/2024
+/////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gameplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
